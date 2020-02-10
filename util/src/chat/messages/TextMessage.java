d61cfc24e3a2cae26fb7d596ba3d183eaecb2d10
Evan Fiordeliso <evan.fiordeliso@temple.edu>
2020-02-10 16:00:52 -0500

    Be more consistent on naming (Change all references of "channel" to "room")

--- util/src/chat/messages/TextMessage.java
+++ util/src/chat/messages/TextMessage.java
@@ -4,20 +4,20 @@ import java.util.HashMap;
 
 public class TextMessage extends Message {
     public static final String COMMAND = "TEXT";
-    public static final String DATA_CHANNEL = "channel";
+    public static final String DATA_ROOM = "room";
     public static final String DATA_SENDER_ID = "sender_id";
     public static final String DATA_SENDER_NAME = "sender_name";
     public static final String DATA_RAW_TEXT = "raw_text";
 
-    public TextMessage(String channel, String text) {
+    public TextMessage(String room, String text) {
         super(COMMAND, new HashMap<>() {{
-            put(DATA_CHANNEL, channel);
+            put(DATA_ROOM, room);
             put(DATA_RAW_TEXT, text);
         }});
     }
 
-    public static TextMessage make(String channel, String text) {
-        return new TextMessage(channel, text);
+    public static TextMessage make(String room, String text) {
+        return new TextMessage(room, text);
     }
 
     public static TextMessage make(String text) {
